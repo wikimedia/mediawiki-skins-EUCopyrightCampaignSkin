45c92736d14c87c657083b602ea353d0b90c4348
Umherirrender <umherirrender_de.wp@web.de>
2018-09-07 21:32:14 +0200

    Add stylelint for less files
    Change-Id: Ib3d9f25fbae6ad0250a76859ee0c7e0dbc0fb1cb

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: {
@@ -13,9 +14,16 @@ module.exports = function ( grunt ) {
 				'!node_modules/**',
 				'!vendor/**'
 			]
+		},
+		stylelint: {
+			all: [
+				'**/*.less',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint'  ] );
 	grunt.registerTask( 'default', 'test' );
 };

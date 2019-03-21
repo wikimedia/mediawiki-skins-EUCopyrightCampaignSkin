6f0f5a0ce2394e64206d3acf7c5796e70a59f37a
Kunal Mehta <legoktm@member.fsf.org>
2019-03-20 19:42:07 -0700

    Upgrade to newer phan
    Change-Id: I5cb68492b98d0e2e3c896dbe66325b389eaec8e9

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 // getSkin()->euMemberLanguagesLanguageCodes
 $cfg['suppress_issue_types'][] = 'PhanUndeclaredProperty';
 

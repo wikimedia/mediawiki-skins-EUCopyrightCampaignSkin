21a54a2e7a5342148a58ba36ced43f14348b4ba6
James D. Forrester <jforrester@wikimedia.org>
2019-05-19 13:47:01 +0200

    build: Upgrade mediawiki/mediawiki-phan-config from 0.5.0 to 0.6.0
    Change-Id: Ic5554b5a403cde0a0a3a6313ba4c51a5a838a9b3

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,7 @@
 <?php
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
 // getSkin()->euMemberLanguagesLanguageCodes
 $cfg['suppress_issue_types'][] = 'PhanUndeclaredProperty';
 
